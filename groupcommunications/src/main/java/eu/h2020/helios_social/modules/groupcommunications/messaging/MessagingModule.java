799bf5a7306b0d15d52a9d31b4c9bc0c9504a2b3
vasgat <vasgat@iti.gr>
2021-03-11 16:59:30 +0200

    Add TusClient dependency to allow uploads to personal storage (current version is using ATOS server)

--- groupcommunications/src/main/java/eu/h2020/helios_social/modules/groupcommunications/messaging/MessagingModule.java
+++ groupcommunications/src/main/java/eu/h2020/helios_social/modules/groupcommunications/messaging/MessagingModule.java
@@ -1,24 +1,48 @@
 package eu.h2020.helios_social.modules.groupcommunications.messaging;
 
+import android.app.Application;
+import android.content.SharedPreferences;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
 import javax.inject.Singleton;
 
 import dagger.Module;
 import dagger.Provides;
 import eu.h2020.helios_social.modules.groupcommunications.api.messaging.MessageTracker;
 import eu.h2020.helios_social.modules.groupcommunications.api.messaging.MessagingManager;
+import io.tus.android.client.TusPreferencesURLStore;
+import io.tus.java.client.TusClient;
 
 @Module
 public class MessagingModule {
 
-	@Provides
-	@Singleton
-	MessageTracker providesMessageTracker(MessageTrackerImpl messageTracker) {
-		return messageTracker;
-	}
+    @Provides
+    @Singleton
+    MessageTracker providesMessageTracker(MessageTrackerImpl messageTracker) {
+        return messageTracker;
+    }
 
-	@Provides
-	@Singleton
-	MessagingManager providesMessagingManager(MessagingManagerImpl messageTracker) {
-		return messageTracker;
-	}
+    @Provides
+    @Singleton
+    MessagingManager providesMessagingManager(MessagingManagerImpl messageTracker) {
+        return messageTracker;
+    }
+
+    @Provides
+    @Singleton
+    TusClient providesTusClient(Application app) {
+        SharedPreferences pref = app.getSharedPreferences("tus", 0);
+        TusClient client = new TusClient();
+        String url = app.getString(eu.h2020.helios_social.modules.filetransfer.R.string.TUS_URL);
+        try {
+            client.setUploadCreationURL(new URL(url));
+            client.enableResuming(new TusPreferencesURLStore(pref));
+            return client;
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 }
