cec08f254b071e5ec3c75e57766e47be237f1c44
vasgat <vasgat@iti.gr>
2021-03-11 17:01:33 +0200

    Add WorkManager and ContentAwareProfileManager dependencies

--- groupcommunications/src/main/java/eu/h2020/helios_social/modules/groupcommunications/mining/MiningModule.java
+++ groupcommunications/src/main/java/eu/h2020/helios_social/modules/groupcommunications/mining/MiningModule.java
@@ -1,15 +1,22 @@
 package eu.h2020.helios_social.modules.groupcommunications.mining;
 
+import android.app.Application;
+
+import androidx.work.Configuration;
+import androidx.work.WorkManager;
+
+import org.jetbrains.annotations.NotNull;
+
 import javax.inject.Inject;
 import javax.inject.Singleton;
 
 import dagger.Module;
 import dagger.Provides;
 import eu.h2020.helios_social.core.contextualegonetwork.ContextualEgoNetwork;
+import eu.h2020.helios_social.modules.contentawareprofiling.ContentAwareProfileManager;
 import eu.h2020.helios_social.modules.groupcommunications.api.mining.MiningManager;
 import eu.h2020.helios_social.modules.socialgraphmining.GNN.GNNMiner;
 import eu.h2020.helios_social.modules.socialgraphmining.SocialGraphMiner;
-import eu.h2020.helios_social.modules.socialgraphmining.SwitchableMiner;
 
 @Module
 public class MiningModule {
@@ -25,5 +32,22 @@ public class MiningModule {
     MiningManager provideMiningManager(
             MiningManagerImpl miningManager) {
         return miningManager;
+    }
+
+    @Provides
+    @Singleton
+    ContentAwareProfileManager provideContentAwareProfileManager(@NotNull Application app,
+                                                                 ContextualEgoNetwork egoNetwork) {
+        return new ContentAwareProfileManager(app.getApplicationContext(), egoNetwork);
+    }
+
+    @Provides
+    @Singleton
+    WorkManager provideWorkManager(Application app, CustomWorkerFactory customWorkerFactory) {
+        Configuration config = new Configuration.Builder()
+                .setWorkerFactory(customWorkerFactory)
+                .build();
+        WorkManager.initialize(app, config);
+        return WorkManager.getInstance(app);
     }
 }
