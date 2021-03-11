622ac1230de1cd42cbc912d439cbac2258e813d5
vasgat <vasgat@iti.gr>
2021-03-11 17:10:23 +0200

    Allow receiving Image Attachment Messages

--- groupcommunications/src/main/java/eu/h2020/helios_social/modules/groupcommunications/privateconversation/PrivateMessageReceiver.java
+++ groupcommunications/src/main/java/eu/h2020/helios_social/modules/groupcommunications/privateconversation/PrivateMessageReceiver.java
@@ -1,6 +1,14 @@
 package eu.h2020.helios_social.modules.groupcommunications.privateconversation;
 
+import android.app.Application;
+import android.app.DownloadManager;
+import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.database.Cursor;
+import android.net.Uri;
+import android.webkit.MimeTypeMap;
 
 import com.google.gson.Gson;
 
@@ -12,7 +20,7 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
-import java.util.UUID;
+import java.util.List;
 import java.util.logging.Logger;
 
 import javax.inject.Inject;
@@ -21,10 +29,15 @@ import eu.h2020.helios_social.core.contextualegonetwork.ContextualEgoNetwork;
 import eu.h2020.helios_social.core.contextualegonetwork.Interaction;
 import eu.h2020.helios_social.core.messaging_nodejslibp2p.HeliosMessagingReceiver;
 import eu.h2020.helios_social.core.messaging_nodejslibp2p.HeliosNetworkAddress;
-import eu.h2020.helios_social.happ.helios.talk.api.db.DatabaseComponent;
-import eu.h2020.helios_social.happ.helios.talk.api.db.Transaction;
-import eu.h2020.helios_social.happ.helios.talk.api.event.EventBus;
-import eu.h2020.helios_social.happ.helios.talk.api.sync.event.AckMessageEvent;
+import eu.h2020.helios_social.modules.groupcommunications.api.exception.FormatException;
+import eu.h2020.helios_social.modules.groupcommunications.api.messaging.Attachment;
+import eu.h2020.helios_social.modules.groupcommunications_utils.data.BdfDictionary;
+import eu.h2020.helios_social.modules.groupcommunications_utils.data.BdfList;
+import eu.h2020.helios_social.modules.groupcommunications_utils.data.Encoder;
+import eu.h2020.helios_social.modules.groupcommunications_utils.db.DatabaseComponent;
+import eu.h2020.helios_social.modules.groupcommunications_utils.db.Transaction;
+import eu.h2020.helios_social.modules.groupcommunications_utils.sync.event.EventBus;
+import eu.h2020.helios_social.modules.groupcommunications_utils.sync.event.AckMessageEvent;
 import eu.h2020.helios_social.modules.groupcommunications.api.contact.ContactId;
 import eu.h2020.helios_social.modules.groupcommunications.api.context.DBContext;
 import eu.h2020.helios_social.modules.groupcommunications.api.exception.DbException;
@@ -37,7 +50,9 @@ import eu.h2020.helios_social.modules.groupcommunications.api.mining.MiningManag
 import eu.h2020.helios_social.modules.groupcommunications.messaging.event.PrivateMessageReceivedEvent;
 import eu.h2020.helios_social.modules.socialgraphmining.SocialGraphMiner;
 
+import static android.content.Context.DOWNLOAD_SERVICE;
 import static eu.h2020.helios_social.modules.groupcommunications.api.CommunicationConstants.PRIVATE_MESSAGE_PROTOCOL;
+import static eu.h2020.helios_social.modules.groupcommunications.api.messaging.MessageConstants.ATTACHMENTS;
 import static java.util.logging.Logger.getLogger;
 
 public class PrivateMessageReceiver
@@ -50,15 +65,19 @@ public class PrivateMessageReceiver
     private final MiningManager miningManager;
     private final MessageTracker messageTracker;
     private final EventBus eventBus;
+    private final Application app;
+    private final Encoder encoder;
 
     @Inject
-    public PrivateMessageReceiver(DatabaseComponent db,
+    public PrivateMessageReceiver(Application app, DatabaseComponent db,
                                   ContextualEgoNetwork egoNetwork, MiningManager miningManager,
-                                  MessageTracker messageTracker, EventBus eventBus) {
+                                  MessageTracker messageTracker, Encoder encoder, EventBus eventBus) {
+        this.app = app;
         this.db = db;
         this.egoNetwork = egoNetwork;
         this.miningManager = miningManager;
         this.messageTracker = messageTracker;
+        this.encoder = encoder;
         this.eventBus = eventBus;
     }
 
@@ -121,23 +140,75 @@ public class PrivateMessageReceiver
                     MessageState.DELIVERED,
                     true,
                     false,
-                    privateMessage.getMessageType());
+                    privateMessage.getMessageType(),
+                    privateMessage.getMessageBody() != null);
             messageTracker.trackIncomingMessage(txn, privateMessage);
 
             LOG.info("received ack preferences: " + privateMessage.getPreferences());
             if (privateMessage.getPreferences() != null)
                 ackMessage(txn, contactId, privateMessage);
-            eventBus.broadcast(
-                    new PrivateMessageReceivedEvent(messageHeader, contactId));
             db.commitTransaction(txn);
+
+
+            MimeTypeMap mime = MimeTypeMap.getSingleton();
+            DownloadManager downloadmanager = (DownloadManager) app.getSystemService(DOWNLOAD_SERVICE);
+            List<Long> ids = new ArrayList<>();
+            if (messageHeader.getMessageType() == Message.Type.IMAGES) {
+                for (Attachment a : privateMessage.getAttachments()) {
+                    String path = "/" + a.getUrl().replaceAll(".*/", "") + "." + mime.getExtensionFromMimeType(a.getContentType());
+                    Uri uri = Uri.parse(a.getUrl());
+                    DownloadManager.Request request = new DownloadManager.Request(uri);
+                    request.setMimeType(a.getContentType());
+                    request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_HIDDEN);
+
+                    LOG.info("Downloading attachment started! " + path);
+
+                    request.setDestinationInExternalFilesDir(app.getApplicationContext(), "/data", path);
+
+                    ids.add(downloadmanager.enqueue(request));
+                }
+
+
+                BroadcastReceiver onComplete = new BroadcastReceiver() {
+                    @Override
+                    public void onReceive(Context arg0, Intent intent) {
+
+                        Long downloadId = intent.getLongExtra(
+                                DownloadManager.EXTRA_DOWNLOAD_ID, 0);
+                        LOG.info("Download completed to id " + downloadId);
+                        privateMessage.getAttachments().get(ids.indexOf(downloadId)).setUri(downloadmanager.getUriForDownloadedFile(downloadId).toString());
+                        DownloadManager.Query query = new DownloadManager.Query();
+                        query.setFilterByStatus(DownloadManager.STATUS_PAUSED |
+                                DownloadManager.STATUS_PENDING |
+                                DownloadManager.STATUS_RUNNING);
+                        Cursor cursor = downloadmanager.query(query);
+                        if (cursor != null && cursor.getCount() > 0) {
+                            return;
+                        } else {
+                            try {
+                                addAttachmentMetadata(privateMessage.getId(), privateMessage.getAttachments());
+                            } catch (DbException e) {
+                                e.printStackTrace();
+                            }
+                            eventBus.broadcast(
+                                    new PrivateMessageReceivedEvent(messageHeader, contactId));
+                        }
+                    }
+                };
+                app.registerReceiver(onComplete, new IntentFilter(DownloadManager.ACTION_DOWNLOAD_COMPLETE));
+            } else {
+                eventBus.broadcast(
+                        new PrivateMessageReceivedEvent(messageHeader, contactId));
+            }
         } finally {
             db.endTransaction(txn);
         }
     }
 
-    private void onReceiveAckMessage(ContactId contactId, Message ack) {
+    private void onReceiveAckMessage(ContactId contactId, Message ack) throws DbException {
+        String[] fields = ack.getMessageBody().split("%");
         ArrayList<Interaction> interactions = egoNetwork
-                .getCurrentContext()
+                .getOrCreateContext(fields[0] + "%" + fields[1])
                 .getOrAddEdge(
                         egoNetwork.getEgo(),
                         egoNetwork.getOrCreateNode(contactId.getId(), null))
@@ -146,6 +217,7 @@ public class PrivateMessageReceiver
         LOG.info("ack preferences: " + ack.getPreferences());
         miningManager.getSocialGraphMiner().newInteraction(interaction, ack.getPreferences(),
                 SocialGraphMiner.InteractionType.RECEIVE_REPLY);
+        messageTracker.setDeliveredFlag(fields[2]);
 
     }
 
@@ -169,8 +241,26 @@ public class PrivateMessageReceiver
                 .getSocialGraphMiner()
                 .getModelParameters(interaction);
         LOG.info("send ack_preferences: " + ack_preferences);
-        Message ack = new Message(ack_preferences, context.getName() + "%" + context.getId());
-
+        Message ack = new Message(ack_preferences, context.getName() + "%" + context.getId() + "%" + privateMessage.getId());
         eventBus.broadcast(new AckMessageEvent(contactId, ack));
+    }
+
+    private void addAttachmentMetadata(String messageId,
+                                       List<Attachment> attachments) throws DbException {
+        Transaction txn = db.startTransaction(false);
+        try {
+            BdfDictionary meta = new BdfDictionary();
+            BdfList attachmentList = new BdfList();
+            for (Attachment a : attachments) {
+                attachmentList.add(BdfList.of(a.getUri(), a.getUrl(), a.getContentType()));
+            }
+            meta.put(ATTACHMENTS, attachmentList);
+            db.mergeMessageMetadata(txn, messageId, encoder.encodeMetadata(meta));
+            db.commitTransaction(txn);
+        } catch (FormatException e) {
+            e.printStackTrace();
+        } finally {
+            db.endTransaction(txn);
+        }
     }
 }
