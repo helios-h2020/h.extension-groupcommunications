3e0d5cbf495d854a20bb21745dba62e8c6344769
vasgat <vasgat@iti.gr>
2021-03-11 17:05:23 +0200

    Update MiningManager to listen to battery events in order to start Content Aware Profiling work when the phone start charging.

--- groupcommunications/src/main/java/eu/h2020/helios_social/modules/groupcommunications/mining/MiningManagerImpl.java
+++ groupcommunications/src/main/java/eu/h2020/helios_social/modules/groupcommunications/mining/MiningManagerImpl.java
@@ -1,8 +1,16 @@
 package eu.h2020.helios_social.modules.groupcommunications.mining;
 
+import androidx.work.Constraints;
+import androidx.work.Data;
+import androidx.work.ExistingWorkPolicy;
+import androidx.work.OneTimeWorkRequest;
+import androidx.work.WorkManager;
+import androidx.work.WorkRequest;
+
 import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.logging.Logger;
 import java.util.stream.Collectors;
 
 import javax.annotation.concurrent.Immutable;
@@ -10,23 +18,47 @@ import javax.inject.Inject;
 
 import eu.h2020.helios_social.core.contextualegonetwork.ContextualEgoNetwork;
 import eu.h2020.helios_social.core.contextualegonetwork.Node;
-import eu.h2020.helios_social.happ.helios.talk.api.nullsafety.NotNullByDefault;
+import eu.h2020.helios_social.modules.groupcommunications_utils.battery.event.BatteryEvent;
+import eu.h2020.helios_social.modules.groupcommunications_utils.sync.event.Event;
+import eu.h2020.helios_social.modules.groupcommunications_utils.sync.event.EventBus;
+import eu.h2020.helios_social.modules.groupcommunications_utils.sync.event.EventListener;
+import eu.h2020.helios_social.modules.groupcommunications_utils.nullsafety.NotNullByDefault;
+import eu.h2020.helios_social.modules.groupcommunications_utils.settings.Settings;
+import eu.h2020.helios_social.modules.groupcommunications_utils.settings.SettingsManager;
+import eu.h2020.helios_social.modules.contentawareprofiling.model.ModelType;
+import eu.h2020.helios_social.modules.groupcommunications.api.exception.DbException;
+import eu.h2020.helios_social.modules.groupcommunications.api.mining.ContentAwareProfilingType;
 import eu.h2020.helios_social.modules.groupcommunications.api.mining.MiningManager;
-import eu.h2020.helios_social.modules.socialgraphmining.GNN.GNNMiner;
 import eu.h2020.helios_social.modules.socialgraphmining.SocialGraphMiner;
-import eu.h2020.helios_social.modules.socialgraphmining.SwitchableMiner;
+
+import static eu.h2020.helios_social.modules.groupcommunications_utils.settings.SettingsConsts.PREF_CONTENT_PROFILING;
+import static eu.h2020.helios_social.modules.groupcommunications_utils.settings.SettingsConsts.SETTINGS_NAMESPACE;
+import static java.util.logging.Logger.getLogger;
 
 @Immutable
 @NotNullByDefault
-public class MiningManagerImpl implements MiningManager {
+public class MiningManagerImpl implements MiningManager, EventListener {
+    private static String TAG = MiningManagerImpl.class.getName();
+    private final static Logger LOG = getLogger(TAG);
 
     private final SocialGraphMiner socialGraphMiner;
     private final ContextualEgoNetwork egoNetwork;
+    private final SettingsManager settingsManager;
+    private final WorkManager workManager;
+    private final EventBus eventBus;
 
     @Inject
-    MiningManagerImpl(SocialGraphMiner socialGraphMiner, ContextualEgoNetwork egoNetwork) {
+    public MiningManagerImpl(SocialGraphMiner socialGraphMiner,
+                             ContextualEgoNetwork egoNetwork,
+                             SettingsManager settingsManager,
+                             WorkManager workManager,
+                             EventBus eventBus) {
         this.socialGraphMiner = socialGraphMiner;
         this.egoNetwork = egoNetwork;
+        this.settingsManager = settingsManager;
+        this.workManager = workManager;
+        this.eventBus = eventBus;
+        this.eventBus.addListener(this);
     }
 
     @Override
@@ -61,4 +93,50 @@ public class MiningManagerImpl implements MiningManager {
                         (oldValue, newValue) -> oldValue,
                         LinkedHashMap::new));
     }
+
+
+    @Override
+    public void eventOccurred(Event e) {
+        if (e instanceof BatteryEvent && ((BatteryEvent) e).isCharging()) {
+            try {
+                LOG.info("Battery is charging!");
+                Settings s = settingsManager.getSettings(SETTINGS_NAMESPACE);
+                ContentAwareProfilingType profilingType = ContentAwareProfilingType.fromValue(s.getInt(PREF_CONTENT_PROFILING, 0));
+
+                if (profilingType == ContentAwareProfilingType.COARSE_INTEREST_RPOFILE) {
+                    LOG.info("Selected Profiling Type: " + profilingType);
+                    Constraints constraints = new Constraints.Builder()
+                            .build();
+                    WorkRequest request = new OneTimeWorkRequest.Builder(ProfilingWorker.class).setConstraints(constraints)
+                            .setInputData(new Data.Builder().putString("MODEL", ModelType.COARSE.toString()).build()).build();
+
+                    workManager.enqueueUniqueWork(
+                            "ContentAwareProfiler",
+                            ExistingWorkPolicy.KEEP,
+                            (OneTimeWorkRequest) request
+                    );
+
+                    LOG.info("Work request with id " + request.getId() + " enqueued!");
+                } else if (profilingType == ContentAwareProfilingType.FINE_INTEREST_PROFILE) {
+                    LOG.info("Selected Profiling Type: " + profilingType);
+
+                    Constraints constraints = new Constraints.Builder()
+                            .build();
+                    WorkRequest request = new OneTimeWorkRequest.Builder(ProfilingWorker.class).setConstraints(constraints)
+                            .setInputData(new Data.Builder().putString("MODEL", ModelType.FINE.toString()).build()).build();
+
+                    workManager.beginUniqueWork(
+                            "ContentAwareProfiler",
+                            ExistingWorkPolicy.KEEP,
+                            (OneTimeWorkRequest) request
+                    );
+
+                    LOG.info("Work request with id " + request.getId() + " enqueued!");
+                }
+            } catch (DbException dbException) {
+                dbException.printStackTrace();
+            }
+        }
+    }
+
 }
