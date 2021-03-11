734940168f9ea579990fa3ec4df77a436c64aad6
vasgat <vasgat@iti.gr>
2021-03-11 16:38:13 +0200

    Allow the creation of Image Attachment Messages

--- groupcommunications/src/main/java/eu/h2020/helios_social/modules/groupcommunications/group/GroupMessageFactoryImpl.java
+++ groupcommunications/src/main/java/eu/h2020/helios_social/modules/groupcommunications/group/GroupMessageFactoryImpl.java
@@ -1,12 +1,13 @@
 package eu.h2020.helios_social.modules.groupcommunications.group;
 
-import java.time.Clock;
+import java.util.List;
 import java.util.UUID;
 
 import javax.inject.Inject;
 
-import eu.h2020.helios_social.happ.helios.talk.api.identity.Identity;
-import eu.h2020.helios_social.happ.helios.talk.api.identity.IdentityManager;
+import eu.h2020.helios_social.modules.groupcommunications.api.messaging.Attachment;
+import eu.h2020.helios_social.modules.groupcommunications_utils.identity.Identity;
+import eu.h2020.helios_social.modules.groupcommunications_utils.identity.IdentityManager;
 import eu.h2020.helios_social.modules.groupcommunications.api.messaging.GroupMessage;
 import eu.h2020.helios_social.modules.groupcommunications.api.messaging.Message;
 import eu.h2020.helios_social.modules.groupcommunications.api.peer.PeerId;
@@ -14,29 +15,47 @@ import eu.h2020.helios_social.modules.groupcommunications.api.peer.PeerInfo;
 import eu.h2020.helios_social.modules.groupcommunications.api.privategroup.GroupMessageFactory;
 
 public class GroupMessageFactoryImpl implements
-		GroupMessageFactory {
+        GroupMessageFactory {
 
-	private final IdentityManager identityManager;
+    private final IdentityManager identityManager;
 
-	@Inject
-	public GroupMessageFactoryImpl(IdentityManager identityManager) {
-		this.identityManager = identityManager;
-	}
+    @Inject
+    public GroupMessageFactoryImpl(IdentityManager identityManager) {
+        this.identityManager = identityManager;
+    }
 
-	@Override
-	public GroupMessage createGroupMessage(String groupId,
-			String text, long timestamp, String funnyName, String fakeId) {
-		Identity identity = identityManager.getIdentity();
-		PeerInfo peerInfo;
-		if (funnyName == null)
-			peerInfo = new PeerInfo.Builder()
-					.peerId(new PeerId(identity.getNetworkId(), fakeId))
-					.alias(identity.getAlias()).build();
-		else
-			peerInfo = new PeerInfo.Builder()
-					.peerId(new PeerId(null, fakeId))
-					.funny_name(funnyName).build();
-		return new GroupMessage(UUID.randomUUID().toString(), groupId,
-				timestamp, text, Message.Type.TEXT, peerInfo);
-	}
+    @Override
+    public GroupMessage createGroupMessage(String groupId,
+                                           String text, long timestamp, String funnyName, String fakeId) {
+        Identity identity = identityManager.getIdentity();
+        PeerInfo peerInfo;
+        if (funnyName == null)
+            peerInfo = new PeerInfo.Builder()
+                    .peerId(new PeerId(identity.getNetworkId(), fakeId))
+                    .alias(identity.getAlias()).build();
+        else
+            peerInfo = new PeerInfo.Builder()
+                    .peerId(new PeerId(null, fakeId))
+                    .funny_name(funnyName).build();
+        return new GroupMessage(UUID.randomUUID().toString(), groupId,
+                timestamp, text, Message.Type.TEXT, peerInfo);
+    }
+
+    @Override
+    public GroupMessage createImageAttachmentMessage(String groupId, List<Attachment> attachments,
+                                                     String text, long timestamp, String funnyName, String fakeId) {
+        Identity identity = identityManager.getIdentity();
+        PeerInfo peerInfo;
+        if (funnyName == null)
+            peerInfo = new PeerInfo.Builder()
+                    .peerId(new PeerId(identity.getNetworkId(), fakeId))
+                    .alias(identity.getAlias()).build();
+        else
+            peerInfo = new PeerInfo.Builder()
+                    .peerId(new PeerId(null, fakeId))
+                    .funny_name(funnyName).build();
+
+        return new GroupMessage(UUID.randomUUID().toString(), groupId,
+                timestamp, text, Message.Type.IMAGES, attachments, peerInfo);
+    }
 }
