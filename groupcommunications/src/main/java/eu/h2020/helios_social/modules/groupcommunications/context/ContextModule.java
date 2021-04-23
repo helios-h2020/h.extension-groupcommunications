65250991fd56f9bc91d3fcbe759e608047c3c114
vasgat <vasgat@iti.gr>
2021-04-23 18:02:48 +0300

    Define ContextManager as an EagerSingleton

--- groupcommunications/src/main/java/eu/h2020/helios_social/modules/groupcommunications/context/ContextModule.java
+++ groupcommunications/src/main/java/eu/h2020/helios_social/modules/groupcommunications/context/ContextModule.java
@@ -9,36 +9,38 @@ import eu.h2020.helios_social.modules.groupcommunications.api.context.sharing.Co
 import eu.h2020.helios_social.modules.groupcommunications.api.context.sharing.SharingContextManager;
 import eu.h2020.helios_social.modules.groupcommunications.context.sharing.ContextInvitationFactoryImpl;
 import eu.h2020.helios_social.modules.groupcommunications.context.sharing.SharingContextManagerImpl;
+import eu.h2020.helios_social.modules.groupcommunications_utils.sync.event.EventBus;
 
 @Module
 public class ContextModule {
-	public static class EagerSingletons {
-		@Inject
-		ContextManager contextManager;
-	}
+    public static class EagerSingletons {
+        @Inject
+        ContextManager contextManager;
+    }
 
-	@Provides
-	@Singleton
-	ContextManager provideContextManager(ContextManagerImpl contextManager) {
-		return contextManager;
-	}
+    @Provides
+    @Singleton
+    ContextManager provideContextManager(ContextManagerImpl contextManager) {
+        return contextManager;
+    }
 
-	@Provides
-	ContextFactory providesContextFactory(ContextFactoryImpl contextFactory){
-		return contextFactory;
-	}
+    @Provides
+    ContextFactory providesContextFactory(ContextFactoryImpl contextFactory) {
+        return contextFactory;
+    }
 
-	@Provides
-	ContextInvitationFactory providesContextInvitationFactory(
-			ContextInvitationFactoryImpl contextInvitationFactory){
-		return contextInvitationFactory;
-	}
+    @Provides
+    ContextInvitationFactory providesContextInvitationFactory(
+            ContextInvitationFactoryImpl contextInvitationFactory) {
+        return contextInvitationFactory;
+    }
 
-	@Provides
-	@Singleton
-	SharingContextManager providesSharingContextManager(
-			SharingContextManagerImpl sharingContextManager){
-		return sharingContextManager;
-	}
+    @Provides
+    @Singleton
+    SharingContextManager providesSharingContextManager(EventBus eventBus,
+                                                        SharingContextManagerImpl sharingContextManager) {
+        eventBus.addListener(sharingContextManager);
+        return sharingContextManager;
+    }
 
 }
