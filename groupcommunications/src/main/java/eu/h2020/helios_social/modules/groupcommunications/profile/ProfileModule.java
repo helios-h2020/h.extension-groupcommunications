8ffc7f31e3add6da41db679bbca69d819ccbbe65
vasgat <vasgat@iti.gr>
2021-04-23 17:43:12 +0300

    Define SharingProfileManager as EagerSingleton

--- groupcommunications/src/main/java/eu/h2020/helios_social/modules/groupcommunications/profile/ProfileModule.java
+++ groupcommunications/src/main/java/eu/h2020/helios_social/modules/groupcommunications/profile/ProfileModule.java
@@ -1,5 +1,6 @@
 package eu.h2020.helios_social.modules.groupcommunications.profile;
 
+import javax.inject.Inject;
 import javax.inject.Singleton;
 
 import dagger.Module;
@@ -7,21 +8,27 @@ import dagger.Provides;
 import eu.h2020.helios_social.modules.groupcommunications.api.profile.ProfileManager;
 import eu.h2020.helios_social.modules.groupcommunications.api.profile.sharing.SharingProfileManager;
 import eu.h2020.helios_social.modules.groupcommunications.profile.sharing.SharingProfileManagerImpl;
+import eu.h2020.helios_social.modules.groupcommunications_utils.sync.event.EventBus;
 
 @Module
 public class ProfileModule {
 
+    public static class EagerSingletons {
+        @Inject
+        SharingProfileManager sharingProfileManager;
+    }
+
     @Provides
     @Singleton
-    ProfileManager providesProfileManager(
-            ProfileManagerImpl profileManager) {
+    ProfileManager providesProfileManager(ProfileManagerImpl profileManager) {
         return profileManager;
     }
 
     @Provides
     @Singleton
-    SharingProfileManager providesSharingProfileManager(
-            SharingProfileManagerImpl sharingProfileManager) {
+    SharingProfileManager providesSharingProfileManager(EventBus eventBus,
+                                                        SharingProfileManagerImpl sharingProfileManager) {
+        eventBus.addListener(sharingProfileManager);
         return sharingProfileManager;
     }
 }
