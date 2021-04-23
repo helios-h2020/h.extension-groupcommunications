078eef435ebd8e8610fddc52fc9aaf26128cd6da
vasgat <vasgat@iti.gr>
2021-04-23 18:07:25 +0300

    Define ContextualEgoNetwork as EagerSingleton, Add ResourceDiscoveryModule and register receivers in the communitation manager for three additional protocols.

--- groupcommunications/src/main/java/eu/h2020/helios_social/modules/groupcommunications/GroupCommunicationsModule.java
+++ groupcommunications/src/main/java/eu/h2020/helios_social/modules/groupcommunications/GroupCommunicationsModule.java
@@ -14,6 +14,10 @@ import eu.h2020.helios_social.core.contextualegonetwork.listeners.CreationListen
 import eu.h2020.helios_social.core.contextualegonetwork.listeners.LoggingListener;
 import eu.h2020.helios_social.core.contextualegonetwork.listeners.RecoveryListener;
 import eu.h2020.helios_social.core.contextualegonetwork.storage.LegacyStorage;
+import eu.h2020.helios_social.modules.groupcommunications.attachment.AttachmentModule;
+import eu.h2020.helios_social.modules.groupcommunications.resourcediscovery.ResourceDiscoveryModule;
+import eu.h2020.helios_social.modules.groupcommunications.resourcediscovery.queries.QueryReceiver;
+import eu.h2020.helios_social.modules.groupcommunications.resourcediscovery.queries.QueryResponseReceiver;
 import eu.h2020.helios_social.modules.groupcommunications_utils.lifecycle.LifecycleManager;
 import eu.h2020.helios_social.modules.groupcommunications.api.CommunicationManager;
 import eu.h2020.helios_social.modules.groupcommunications.contact.ContactModule;
@@ -33,18 +37,24 @@ import eu.h2020.helios_social.modules.groupcommunications.privategroup.PrivateGr
 import eu.h2020.helios_social.modules.groupcommunications.profile.ProfileModule;
 import eu.h2020.helios_social.modules.groupcommunications.profile.sharing.ProfileRequestReceiver;
 import eu.h2020.helios_social.modules.groupcommunications.utils.InternalStorageConfig;
+import eu.h2020.helios_social.modules.socialgraphmining.GNN.GNNMiner;
+import eu.h2020.helios_social.modules.socialgraphmining.GNN.operations.Optimizer;
 
 import static eu.h2020.helios_social.modules.groupcommunications.api.CommunicationConstants.CONTEXT_INVITE_PROTOCOL;
 import static eu.h2020.helios_social.modules.groupcommunications.api.CommunicationConstants.CONTEXT_INVITE_RESPONSE_PROTOCOL;
 import static eu.h2020.helios_social.modules.groupcommunications.api.CommunicationConstants.FORUM_MEMBERSHIP_PROTOCOL;
 import static eu.h2020.helios_social.modules.groupcommunications.api.CommunicationConstants.GROUP_INVITE_PROTOCOL;
 import static eu.h2020.helios_social.modules.groupcommunications.api.CommunicationConstants.GROUP_INVITE_RESPONSE_PROTOCOL;
+import static eu.h2020.helios_social.modules.groupcommunications.api.CommunicationConstants.LOCATION_QUERY_PROTOCOL;
 import static eu.h2020.helios_social.modules.groupcommunications.api.CommunicationConstants.PRIVATE_MESSAGE_PROTOCOL;
+import static eu.h2020.helios_social.modules.groupcommunications.api.CommunicationConstants.QUERY_RESPONSE_PROTOCOL;
 import static eu.h2020.helios_social.modules.groupcommunications.api.CommunicationConstants.REQUEST_PROTOCOL;
 import static eu.h2020.helios_social.modules.groupcommunications.api.CommunicationConstants.RESPONSE_PROTOCOL;
+import static eu.h2020.helios_social.modules.groupcommunications.api.CommunicationConstants.TEXT_QUERY_PROTOCOL;
 import static eu.h2020.helios_social.modules.groupcommunications.api.contact.connection.ConnectionConstants.CONNECTIONS_RECEIVER_ID;
 
 @Module(includes = {
+        AttachmentModule.class,
         ContactModule.class,
         ProfileModule.class,
         PrivateGroupModule.class,
@@ -54,6 +64,7 @@ import static eu.h2020.helios_social.modules.groupcommunications.api.contact.con
         ConversationModule.class,
         ContextModule.class,
         MiningModule.class,
+        ResourceDiscoveryModule.class,
         HeliosEventModule.class
 })
 public class GroupCommunicationsModule {
@@ -61,19 +72,23 @@ public class GroupCommunicationsModule {
     public static class EagerSingletons {
         @Inject
         CommunicationManager communicationManager;
+        @Inject
+        ContextualEgoNetwork egoNetwork;
     }
 
     @Provides
     @Singleton
     CommunicationManager provideCommunicationManager(
             LifecycleManager lifecycleManager,
-            CommunicationManagerImpl communicationManager,
+            ReliableCommunicationManagerImpl communicationManager,
             ConnectionRequestReceiver connectionRequestReceiver,
             PrivateMessageReceiver privateMessageReceiver,
             ContextInvitationReceiver contextInvitationReceiver,
             GroupInvitationReceiver groupInvitationReceiver,
             MembershipReceiver membershipReceiver,
-            ProfileRequestReceiver requestReceiver) {
+            ProfileRequestReceiver requestReceiver,
+            QueryReceiver queryReceiver,
+            QueryResponseReceiver queryResponseReceiver) {
         communicationManager.registerReceiver(CONNECTIONS_RECEIVER_ID,
                 connectionRequestReceiver);
         communicationManager.registerReceiver(PRIVATE_MESSAGE_PROTOCOL,
@@ -91,6 +106,9 @@ public class GroupCommunicationsModule {
                 membershipReceiver);
         communicationManager.registerReceiver(REQUEST_PROTOCOL, requestReceiver);
         communicationManager.registerReceiver(RESPONSE_PROTOCOL, requestReceiver);
+        communicationManager.registerReceiver(TEXT_QUERY_PROTOCOL, queryReceiver);
+        communicationManager.registerReceiver(LOCATION_QUERY_PROTOCOL, queryReceiver);
+        communicationManager.registerReceiver(QUERY_RESPONSE_PROTOCOL, queryResponseReceiver);
         lifecycleManager.registerOpenDatabaseHook(communicationManager);
         lifecycleManager.registerService(communicationManager);
         return communicationManager;
@@ -104,12 +122,16 @@ public class GroupCommunicationsModule {
         ContextualEgoNetwork egoNetwork = ContextualEgoNetwork.createOrLoad(
                 new LegacyStorage(config.getStorageDir().getPath().toString() +
                         File.separator), "ego", "null");
+        GNNMiner.class.getDeclaredConstructors();
         egoNetwork.addListener(
                 new RecoveryListener());//automatic saving with minimal overhead
         egoNetwork.addListener(new CreationListener());//keep timestamps
         egoNetwork.addListener(new LoggingListener());//print events
         //Some needed non-sense
         Interaction.class.getDeclaredConstructors();
+        Optimizer.Adam.class.getDeclaredConstructors();
+        Optimizer.Regularization.class.getDeclaredConstructors();
+        Optimizer.class.getDeclaredConstructors();
 
         egoNetwork.setCurrent(egoNetwork.getOrCreateContext("All%All"));
         egoNetwork.save();
