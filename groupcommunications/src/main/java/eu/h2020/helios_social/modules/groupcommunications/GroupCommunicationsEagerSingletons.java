3396ec0c7a9eb1ed6f29fea7dbf99827d0cdccb6
vasgat <vasgat@iti.gr>
2021-04-23 18:08:44 +0300

    Declare the defined EagerSingletons from the ResourceDiscoveryModule, MessagingModule, MiningModule, ProfileModule and GroupModule.

--- groupcommunications/src/main/java/eu/h2020/helios_social/modules/groupcommunications/GroupCommunicationsEagerSingletons.java
+++ groupcommunications/src/main/java/eu/h2020/helios_social/modules/groupcommunications/GroupCommunicationsEagerSingletons.java
@@ -1,19 +1,39 @@
 package eu.h2020.helios_social.modules.groupcommunications;
 
 import eu.h2020.helios_social.modules.groupcommunications.context.ContextModule;
+import eu.h2020.helios_social.modules.groupcommunications.group.GroupModule;
+import eu.h2020.helios_social.modules.groupcommunications.messaging.MessagingModule;
+import eu.h2020.helios_social.modules.groupcommunications.mining.MiningModule;
+import eu.h2020.helios_social.modules.groupcommunications.profile.ProfileModule;
+import eu.h2020.helios_social.modules.groupcommunications.resourcediscovery.ResourceDiscoveryModule;
 
 public interface GroupCommunicationsEagerSingletons {
 
-	void inject(GroupCommunicationsModule.EagerSingletons init);
+    void inject(GroupCommunicationsModule.EagerSingletons init);
 
-	void inject(ContextModule.EagerSingletons init);
+    void inject(ContextModule.EagerSingletons init);
 
-	class Helper {
+    void inject(ResourceDiscoveryModule.EagerSingletons init);
 
-		public static void injectEagerSingletons(
-				GroupCommunicationsEagerSingletons c) {
-			c.inject(new ContextModule.EagerSingletons());
-			c.inject(new GroupCommunicationsModule.EagerSingletons());
-		}
-	}
+    void inject(MessagingModule.EagerSingletons init);
+
+    void inject(MiningModule.EagerSingletons init);
+
+    void inject(ProfileModule.EagerSingletons init);
+
+    void inject(GroupModule.EagerSingletons init);
+
+    class Helper {
+
+        public static void injectEagerSingletons(
+                GroupCommunicationsEagerSingletons c) {
+            c.inject(new ContextModule.EagerSingletons());
+            c.inject(new ResourceDiscoveryModule.EagerSingletons());
+            c.inject(new GroupCommunicationsModule.EagerSingletons());
+            c.inject(new MessagingModule.EagerSingletons());
+            c.inject(new MiningModule.EagerSingletons());
+            c.inject(new ProfileModule.EagerSingletons());
+            c.inject(new GroupModule.EagerSingletons());
+        }
+    }
 }
